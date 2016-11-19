507a6d78f4db2e2a0acf3164f3983d5acfec44ae
kojotak <kojotak@beneda>
2016-11-19 22:20:02 +0100

    deleted all Syste.out.printlns

--- src/main/java/cz/kojotak/sreac/SampleController.java
+++ src/main/java/cz/kojotak/sreac/SampleController.java
@@ -12,12 +12,10 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-    	System.out.println("home...");
         return "Hello World!";
     }
 
     public static void main(String[] args) throws Exception {
-    	System.out.println("main...");
         SpringApplication.run(SampleController.class, args);
     }
 }
